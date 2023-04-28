c57f462d5081ac71c060c65f59b33db2d5254a0a
KoblizekXD <janprokupek@centrum.cz>
2023-04-28 19:11:24 +0200

    update

---
+++ src/main/java/com/discord/http/client/DiscordClient.java
@@ -0,0 +1,10 @@
+package com.discord.http.client;
+
+public final class DiscordClient {
+    private final String authToken;
+
+    public DiscordClient(String authToken) {
+        this.authToken = authToken;
+    }
+
+}
